26d2055cb75151d27daf73c3b714b8eec5c73c20
BraveWangDev <BraveWangDev@126.com>
2019-07-26 16:52:23 +0800

    新增Ant-Desgin-vue及组件按需加载

--- src/main.js
+++ src/main.js
@@ -3,6 +3,20 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// 1,全局引入ant-design-vue
+// import Antd from "ant-design-vue";
+// import "ant-design-vue/dist/antd.less";
+// Vue.use(Antd);
+
+// 2,对每个组件进行单独引用
+// import Button from "ant-design-vue/lib/button";
+// import "ant-design-vue/lib/button/style";
+// Vue.use(Button);
+
+// 3,使用bable插件自动实现按需引用组件
+import { Button } from "ant-design-vue";
+Vue.use(Button);
+
 Vue.config.productionTip = false;
 
 new Vue({
